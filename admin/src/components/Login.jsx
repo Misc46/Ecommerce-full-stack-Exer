597086b51a07d0f75b9e25602ddf6a9de433bd02
Misc46 <undyne567@gmail.com>
2025-01-29 02:28:27 +0700

    Creating admin auth

---
+++ admin/src/components/Login.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { backendUrl } from '../App'
+
+const Login = () => {
+
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+
+    const onSubmitHandler = async (e) => {
+        try {
+            e.preventDefault();
+            const response = await axios.post(backendUrl + '/api/user/admin',{email,password});
+            
+
+        } catch (error) {
+            
+        }
+    } 
+
+  return (
+    <div className='min-h-screen flex items-center justify-center w-full'>
+      <div className='bg-white shadow-md rounded-lg px-8 py-6 max-w-md'>
+        <h1 className='text-2xl font-bold mb-4'>Admin Panel</h1>
+        <form onSubmit={onSubmitHandler}>
+            <div className='mb-3 min-w-72'>
+                <p className='text-sm font-medium text-gray-700 mb-2'>Alamat Email</p>
+                <input onChange={(e)=>setEmail(e.target.value)} value={email} className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type='email' placeholder='Email anda' required></input>
+            </div>
+            <div className='mb-3 min-w-72'>
+                <p className='text-sm font-medium text-gray-700 mb-2'>Kata Sandi</p>
+                <input onChange={(e)=>setPassword(e.target.value)} value={password} className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type='password' placeholder='Kata Sandi Anda' required></input>
+            </div>
+            <button className='mt-2 w-full py-2 px-4 rounded-md text-white bg-black cursor-pointer'type='submit'>Masuk</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
